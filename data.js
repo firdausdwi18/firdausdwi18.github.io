70133fe075e5bde122cd9a3f3748d8692fd3667e
firdausdwi18 <153163578+firdausdwi18@users.noreply.github.com>
2023-12-07 13:10:11 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+const nama = "firdaus";
+let usia = 10;
+
+let biodata = document.getElementById('biodata');
+
+function generateBiodata() {
+    let generasi;
+
+    if(usia > 2 && usia < 10) {
+        generasi = "generasi anak anak";
+    }
+    if(usia > 10 && usia < 18) {
+        generasi = "generasi remaja";
+    }
+    else if (usia > 18 && usia < 30) {
+        generasi = "generasi dewasa";
+    }
+    else if (usia >= 30){
+        generasi = "generasi tua";
+    }
+    else {
+        generasi = "generasi bayi";
+    }
+    return biodata.innerHTML = generasi;
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
